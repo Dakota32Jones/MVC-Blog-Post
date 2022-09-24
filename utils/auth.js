43e680e387dae52a2d4b864832069e7deb4abb0f
Dakota Jones <dakota32jones@gmail.com>
2022-09-23 22:58:54 -0500

    added auth helper function

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// creating authentication helper for users who are or who are not logged in.
+const withAuth = (req, res, next) => {
+  if (!req.session.user_id) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
