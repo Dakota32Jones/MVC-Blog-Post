eb1cfdde6af1ab2401ac9d266e51bd551154aa92
Dakota Jones <dakota32jones@gmail.com>
2022-09-24 12:07:53 -0500

    try and get app working on heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: "blue",
   cookie: {},
   resave: true,
   rolling: true,
