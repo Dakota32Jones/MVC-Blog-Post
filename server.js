3fd7682d5b73e9610c04ffb1eb7ddf63467aae95
Dakota Jones <dakota32jones@gmail.com>
2022-09-24 11:25:57 -0500

    added session secret

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: process.env.SESSION_KEY,
+  secret: process.env.SECRET,
   cookie: {},
   resave: true,
   rolling: true,
